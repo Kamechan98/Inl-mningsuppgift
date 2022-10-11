838f55c387e9c2c1958573771d63effb129ca4d4
Evelina Nyman <ina.nyman98@gmail.com>
2022-10-11 15:40:23 +0200

    Nästan klar!

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const form = document.querySelector('#validationForm')
+const form = document.querySelector('#validationForm') 
 const btn = document.querySelector('#btn');
+const div = document.querySelector('#errorMessage') //Kallar på errormessage
+//const div = document.getElementById("errorMessage")
 
 const errors = [];                   // en tom array som fångar upp eventuella fel i formuläret
 
@@ -14,64 +16,107 @@ const setSuccess = (input) => {     // Deklarerar setSuccess som en funktion som
 
 const validateText = (id) => {
     const input = document.querySelector(id)  //Hämtar en referens med hjälp av ett id från html
+    const regExTxt = /^[a-zA-ZäöüßÄÖÜ]$/ //Tillåter namn med alla bokstäver i alfabetet, plus bindestreck
 
     if(input.value.trim() === '') {
-        console.log('You have to write a valid name');
-        return setError(input)                  //Kallar på setError och kör den här funktionen med referens som vi får från id
+        // console.log(input.id + ' You have to write a valid name');
+        console.log(`You have to write a valid ${input.name}`)
+        
+        input.classList.add('error');
+        input.classList.remove('success');
+
+        return setError(input);                  //Kallar på setError och kör den här funktionen med referens som vi får från id
       } 
       else if (input.value.length < 2) {
-        console.log('You have to write a valid name');
-        return setError(input)
+        console.log(`You have to write a valid ${input.name}`)
+        //console.log('You have to write a valid name');
+
+        input.classList.add('error');
+        input.classList.remove('success');
+
+        return setError(input);
       }
       else {
-        console.log('your name is valid')
-        return setSuccess(input)            //Kallar på setSuccess och kör den här funktionen med referens som vi får från id
+        console.log('your name is valid');
+        
+        input.classList.add('success');
+        input.classList.remove('error');
+
+        return setSuccess(input);            //Kallar på setSuccess och kör den här funktionen med referens som vi får från id
       }
     }
 
-const validateEmail = (id) => {
+    const validateEmail = (id) => {
     const email = document.querySelector(id)
 
     const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/
 
     if(email.value.trim() === '') {
         console.log('You have to write a valid email');
+
+        email.classList.add('error')
+        email.classList.remove('success')
+
       return setError(email)
     }
     else if(!regEx.test(email.value)) {
         console.log('You have to write a valid email');
+
+        email.classList.add('error')
+        email.classList.remove('success')
       return setError(email)
     }
     else {
         console.log('your email is valid')
-      return setSuccess(email)
+      
+        email.classList.add('success');
+        email.classList.remove('error');
+        return setSuccess(email)
     }
 }
 
 const validatePassword = (id) => {
     const password = document.querySelector(id)
     const repeatPassword = document.querySelector('#repeatPassword')
+    
+    const pswStr = /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=[^0-9]*[0-9]).{6,}$/; //Ett lösenord måste ha minst 6 karaktärer, en stor och liten bokstav och ett nummer
 
     if(password.value === '') { 
     console.log('Please enter a password')
-    return 
+
+    password.classList.add('error')
+    password.classList.remove('success')
+    return setError(password)
     }
 
-    else if(password.value.length < 6 || password.value.length > 15) {
-    console.log ('Password needs to be at least 6 characters but no more than 15')
+    else if(password.value.length < 6 || password.value.length > 10) {
+    console.log ('Password needs to be at least 6 characters but no more than 10')
+
+    password.classList.add('error')
+    password.classList.remove('success')
     return setError(password)
     }
 
     else if(repeatPassword.value === '') {
         console.log('Please enter confirm password');
+        
+        password.classList.add('error')
+        password.classList.remove('success')
       return setError(password)
     }
     else if(password.value !== repeatPassword.value) {
         console.log('Your passwords do not match');
+
+        password.classList.add('error')
+        password.classList.remove('success')
       return setError(password)
     }
     else{
         console.log('Congrats your passwords match') 
+
+        password.classList.add('success')
+        password.classList.remove('error')
+
         return setSuccess(repeatPassword)
        
     }  
@@ -82,10 +127,17 @@ const validateCheckbox = (id) => {
 
     if(!checkbox.checked) {
         console.log('You need to accept the terms');
+       
+        checkbox.classList.add('error')
+        checkbox.classList.remove('success')
         return setError(checkbox)
       }
       else {
         console.log('You have accepted the terms');
+        
+        checkbox.classList.add('success')
+        checkbox.classList.remove('error')
+        
         return setSuccess(checkbox)
       }
     }
@@ -93,13 +145,13 @@ const validateCheckbox = (id) => {
 form.addEventListener('submit', e => { //ett event som lyssnar efter en 'submit' och sedan skickar det vi skrivit
     e.preventDefault()      //Hindrar sidan från att ladda om
 
-
-//   validateFirstName('#firstName')
-//   validateLastName('#lastName')
+ //validateFirstName('#firstName')
+//validateLastName('#lastName')
 //   validateEmail('#email')
 //   validatePassword('#password')
 //   validatePassword('#repeatPassword')
 //   validateCheckbox('#terms') 
+
 
     for(let i = 0; i < form.length; i++) {  
         const inputId = '#' + form[i].id
@@ -121,6 +173,7 @@ form.addEventListener('submit', e => { //ett event som lyssnar efter en 'submit'
     }
 }
 
+
 // tar id:et från alla inputs och tar in infon som skrivs in i formuläret i ett user object. 
 const user ={
     firstName:(firstName).value,
@@ -131,10 +184,12 @@ const user ={
 console.log(user)
 
 if(errors.includes(false)) {
-    console.log('Something went wrong')
-}
+    console.log('Something went wrong') 
+    div.classList.remove("d-none")    //* Kallar på errorMessage och tar d-none på html'en
+  }
 else {
     console.log('You made it!');
+    div.classList.add("d-none");
 }
 
 
